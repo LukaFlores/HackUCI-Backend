32609fea03a2587e20032ef28d8c85ca11ffba67
andrewras58 <andrewras58@gmail.com>
2022-02-26 14:12:00 -0800

    restructured files

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 from flask import Flask, request, jsonify
 
-from addProfileInfo import addInfo
 from addUser import addUser
 
 cred = credentials.Certificate("hackuci2022-firebase-adminsdk-g1jrv-61f7819fa4.json")
@@ -15,12 +14,13 @@ db = firestore.client()
 app = Flask(__name__)
 @app.route("/add", methods=["POST"])
 def userInfo():
-    addInfo(request.method,request.get_json(),db)
+    addUser(request.method,request.get_json(),db)
 
 
 @app.route("/getUser", methods=["GET"])
 def otherFunction():
-    addUser(request.method,request.get_json(),db)
+    #other code here
+    pass
 
 
 
