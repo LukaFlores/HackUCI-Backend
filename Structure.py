8075b5c1a5423475b1a61dd5c59bdf99ccc5d599
andrewras58 <andrewras58@gmail.com>
2022-02-26 00:52:04 -0800

    added main.py

---
+++ Structure.py
@@ -0,0 +1,4 @@
+#This is the structure of the backend where we store common variables and base classes
+#This will send arguments to other functions and retireve the return items
+if __name__ == '__main__':
+    print('Hello')
