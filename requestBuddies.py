facfb215aeca0815107f55886edeb6fd458ba9eb
andrewras58 <andrewras58@gmail.com>
2022-02-26 18:23:48 -0800

    requestBuddies two streams

--- requestBuddies.py
+++ requestBuddies.py
@@ -2,5 +2,7 @@ def requestBuddies(method, data, db):
     if method == "GET":
         APPUSER = data["ID"]
         users = db.collection("users").where(f"peopleSeen.{APPUSER}", "==", 'Null').limit(49).stream()
-
+        users += db.collection("users").where("userID", "==", APPUSER).stream()
+        for people in users:
+            pass
         return users.to_dict()
