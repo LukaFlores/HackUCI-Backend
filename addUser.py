a84e308f8876d0ec5ae8ff348e9150345a29ed89
RamyaMangu <manguramya@gmail.com>
2022-02-27 00:52:39 -0800

    Fake Profiles added

--- addUser.py
+++ addUser.py
@@ -1,8 +1,30 @@
-def addUser(method,data,db):
+from faker import Faker
+import numpy as np
+
+
+def addUser(method, data, db):
     if method == "POST":
-        set_empty = {'peopleSeen': []}
-        for i in range(10):
-            newUser = db.collection("users").document()
-            newUser.set(set_empty)
+        g = ["Male", "Female", "Non-Binary", "other"]
+        pwt = ["Morning", "Daytime", "Night", "No Preference"]
+        pwl = ["Light", "Medium", "Intense", "No Preference"]
+        pwr = ["One-Workout Stand", "Long time Relationship"]
+        act = ["badminton", "baseball", "basketball", "dance", "football", "frisbee", "golf", "gym", "hiking", "hockey",
+               "martial-arts", "running", "soccer", "softball", "swimming", "table-tennis", "tennis", "volleyball",
+               "weights", "yoga", "other"]
+        pa = [1/21, 1/21, 1/21, 1/21, 1/21, 1/21, 1/21, 1/21, 1/21, 1/21,
+              1/21, 1/21, 1/21, 1/21, 1/21, 1/21, 1/21, 1/21,
+              1/21, 1/21, 1/21]
+        for _ in range(50):
+            fdata = {
+                'Full Name': Faker.name(),
+                'Birthday': Faker.date(),
+                'Gender': np.random.choice(g, p=[0.25, 0.25, 0.25, 0.25]),
+                "Preferred Workout Time": np.random.choice(pwt, p=[0.2, 0.2, 0.2, 0.2]),
+                "Preferred Workout Level": np.random.choice(pwl, p=[0.2, 0.2, 0.2, 0.2]),
+                "Preferred Workout Relationship": np.random.choice(pwr, p=[0.5, 0.5]),
+                "Activities": np.random.choice([act, pa])
+            }
+            newuser = db.collection("users").document()
+            newuser.set(fdata)
         # WE CAN USE A DIFFERENT RETURN STATEMENT. JUST TO LET ME KNOW IT WAS WORKING
         return "<h1>Profile information for {} has been added </h1>"
